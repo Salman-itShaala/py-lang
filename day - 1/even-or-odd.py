45a36f9d1c5fd4e078e0545d12b0980cb69dc571
salman-itShaala <salman.shaikh@itshaala.com>
2025-07-11 18:04:23 +0530

    added py code

---
+++ day - 1/even-or-odd.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number"))
+
+if number % 2 == 0:
+    print("Given number is even")
+else:
+    print("Given number is odd")
---
+++ day - 1/main.py
@@ -0,0 +1,9 @@
+userName = input("Enter your name")
+
+age = input("Enter your age")
+
+ageAfterTenYrs = int(age) + 10
+
+message =  f"{userName} will be {ageAfterTenYrs} years old after 10 yrs"
+
+print(message)
